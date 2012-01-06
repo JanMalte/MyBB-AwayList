79749d64b71bd8e1934a35317ab35959723336e2
malte.gerth <malte.gerth@meteos.de>
2012-01-06 20:16:39 +0100

    added english translation fixed informal german translation

--- inc/languages/deutsch_du/liste.lang.php
+++ inc/languages/deutsch_du/liste.lang.php
@@ -12,7 +12,37 @@
 
 $l['followingErrors'] = 'Folgende Fehler sind aufgetreten:';
 $l['errorNotActive'] = 'Das Plugin wurde nicht aktiviert. Bitte aktiviere das Plugin um diese Seite anzuzeigen.';
+$l['errorNoDisplay'] = 'Die Anzeige der Liste wurde deaktiviert';
+
+$l['addItemSuccessful'] = 'Eintrag erfolgreich hinzugefügt';
+$l['editItemSuccessful'] = 'Eintrag erfolgreich geändert';
+$l['deleteItemSuccessful'] = 'Eintrag erfolgreich gelöscht';
+
+$l['showOnly'] = 'Zeige nur';
+$l['entries'] = 'Einträge';
+$l['whoIsAt'] = 'Wer ist am';
+$l['personsCurrentlyThere'] = 'Folgende Personen sind am';
+$l['in'] = 'in';
 
 $l['back'] = 'Zurück ';
+$l['show'] = 'Anzeigen';
+$l['edit'] = 'Bearbeiten';
+$l['delete'] = 'Löschen';
+$l['addToList'] = 'In die Liste eintragen';
+$l['requiredFields'] = 'Pflichtfelder';
+$l['newItem'] = 'Neuer Eintrag';
+$l['editItem'] = 'Eintrag bearbeiten';
+$l['deleteItem'] = 'Eintrag löschen';
 
 $l['liste'] = 'Aufenthaltsliste';
+$l['name'] = 'Name';
+$l['status'] = 'Status';
+$l['arrival'] = 'Ankunft';
+$l['departure'] = 'Abflug';
+$l['airline'] = 'Airline';
+$l['place'] = 'Urlaubsort';
+$l['hotel'] = 'Hotel';
+$l['phoneAt'] = 'Telefon in';
+$l['action'] = 'Aktion';
+
+#$l[''] = '';
--- inc/languages/english/liste.lang.php
+++ inc/languages/english/liste.lang.php
@@ -11,6 +11,38 @@
  */
 
 $l['followingErrors'] = 'The following errors occurred:';
-$l['errorNotActive'] = 'The plugin is not activated. You will be redirected to the index in 10 seconds.';
+$l['errorNotActive'] = 'The plugin is not activated. Please activate the plugin.';
+$l['errorNoDisplay'] = 'Displaying the list ist disabled';
 
-$l['back'] = 'Go back ';
+$l['addItemSuccessful'] = 'Entry successfully added';
+$l['editItemSuccessful'] = 'Entry successfully edited';
+$l['deleteItemSuccessful'] = 'Entry successfully deleted';
+
+$l['showOnly'] = 'Show only';
+$l['entries'] = 'items';
+$l['whoIsAt'] = 'Who is at';
+$l['personsCurrentlyThere'] = 'The follwing persons are at';
+$l['in'] = 'in';
+
+$l['back'] = 'Back ';
+$l['show'] = 'Show';
+$l['edit'] = 'Edit';
+$l['delete'] = 'Delete';
+$l['addToList'] = 'Add to list';
+$l['requiredFields'] = 'mandantory fields';
+$l['newItem'] = 'New entry';
+$l['editItem'] = 'Edit entry';
+$l['deleteItem'] = 'Delete entry';
+
+$l['liste'] = 'Awaylist';
+$l['name'] = 'Name';
+$l['status'] = 'State';
+$l['arrival'] = 'Arrival';
+$l['departure'] = 'Departure';
+$l['airline'] = 'Airline';
+$l['place'] = 'Place';
+$l['hotel'] = 'Hotel';
+$l['phoneAt'] = 'Phone number in';
+$l['action'] = 'Action';
+
+#$l[''] = '';
