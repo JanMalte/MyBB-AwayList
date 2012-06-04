3d035e7ce5fa56038683cf4ce2224c96e2272e72
Jan Malte Gerth <anmeldungen@malte-gerth.de>
2012-06-04 07:35:23 +0200

    [TASK] added some phpDoc for PHPUnit tests

--- tests-selenium/pageTitelTest.php
+++ tests-selenium/pageTitelTest.php
@@ -1,24 +1,32 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @category    MyBB.Plugins
+ * @package     AwayList
+ * @subpackage  Tests
+ * @author      Malte Gerth <http://www.malte-gerth.de>
+ * @copyright   Copyright (C) Malte Gerth. All rights reserved.
+ * @license     GNU General Public License version 3 or later
  */
-
-
+// include selenium test case
 require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 
 /**
- * Description of newSeleneseTest
- *
- * @author janmalte
+ * default test
+ * 
+ * @category    MyBB.Plugins
+ * @package     AwayList
+ * @subpackage  Tests
+ * @author      Malte Gerth <http://www.malte-gerth.de>
+ * @copyright   Copyright (C) Malte Gerth. All rights reserved.
+ * @license     GNU General Public License version 3 or later
  */
 class pageTitelTest extends PHPUnit_Extensions_SeleniumTestCase
 {
 
     /**
-     *
-     * @var Array browser configuration for selenium tests
+     * browser configuration for selenium tests
+     * 
+     * @var Array
      */
     public static $browsers = array(
         array(
@@ -29,17 +37,26 @@ class pageTitelTest extends PHPUnit_Extensions_SeleniumTestCase
             'timeout' => 300000,
         )
     );
-    
+
+    /**
+     * setup for selenium tests
+     * 
+     * @return void 
+     */
     function setUp()
     {
-        #$this->setBrowser("*chrome");
         $this->setBrowserUrl('http://mybb16.local/');
     }
 
+    /**
+     * test the page title
+     * 
+     * @return void 
+     */
     function testPageTitel()
     {
         $this->open("/");
-        $this->assertEquals('MyBB Plugins',$this->getTitle());
+        $this->assertEquals('MyBB Plugins', $this->getTitle());
     }
 
 }
